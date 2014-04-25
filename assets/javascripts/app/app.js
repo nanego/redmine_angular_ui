f7412abb86dc47f3e0339be4fb79afbf557f4f00
Vincent Robert <vincent.robert@nanego.com>
2014-04-25 14:46:49 +0200

    Preload data before changing routes

--- assets/javascripts/app/app.js
+++ assets/javascripts/app/app.js
@@ -1,17 +1,21 @@
 'use strict';
 
-angular.module('myApp',['ngRoute',
+var app = angular.module('myApp',['ngRoute',
                         'ngResource',
                         'myApp.controllers',
                         'myApp.services',
-                        'myApp.directives' ])
-  .config(function($routeProvider){
+                        'myApp.directives' ]);
+
+app.config(function($routeProvider){
     $routeProvider.when('/', {
       templateUrl: '/plugin_assets/redmine_angular_ui/templates/index.html',
       controller: 'HomeController',
       resolve: {
         session: function(SessionService) {
           return SessionService.getCurrentUser();
+        },
+        issues: function(IssueService) {
+          return IssueService.getLatestIssues()
         }
       }
     })
--- assets/javascripts/app/controllers.js
+++ assets/javascripts/app/controllers.js
@@ -1,11 +1,33 @@
 'use strict';
 
-angular.module('myApp.controllers',[])
-.controller('HomeController',
-  function($scope, session, SessionService, IssueService){
+var app = angular.module('myApp.controllers',[]);
+
+app.controller('HomeController',
+  function($scope, session, issues, SessionService, IssueService){
     $scope.user = session.user;
-    IssueService.getLatestIssues()
-      .then(function(data) {
-        $scope.issues = data;
-      })
+    $scope.issues = issues;
+});
+
+app.controller('AppCtrl', function($scope, $rootScope, $location) {
+  $rootScope.$on("$routeChangeStart", function (event, next, current) {
+    $scope.alertType = "loading alert-info";
+    $scope.alertMessage = "Chargement...";
+    $scope.active = "progress-striped active progress-warning show";
+  });
+  $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
+    $scope.alertType = "alert-success";
+    $scope.alertMessage = "Chargement terminé :)";
+    $scope.active = "progress-success hide";
+
+    $scope.newLocation = $location.path();
+  });
+  $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
+    alert("ROUTE CHANGE ERROR: " + rejection);
+    $scope.alertType = "alert-error";
+    $scope.alertMessage = "Problème lors du chargement :(";
+    $scope.active = "";
+  });
+
+  $scope.alertType = "alert-info";
+  $scope.alertMessage = "Welcome to the AngularJS Proto";
 });
