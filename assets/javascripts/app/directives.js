8eae577ef3d478229584192e6df6e78507849b6e
Vincent Robert <vincent.robert@nanego.com>
2014-05-26 14:41:22 +0200

    Add directive to show a loading spinner

--- assets/javascripts/app/directives.js
+++ assets/javascripts/app/directives.js
@@ -8,3 +8,24 @@ app.directive('mainTabs', function () {
     templateUrl: '/plugin_assets/redmine_angular_ui/templates/directives/main_tabs.html'
   };
 });
+
+app.directive('spinner', function() {
+  return {
+    scope: {
+      scopeVar: "=spinner"
+    },
+    restrict: 'A',
+    replace: false,
+    template: '<img src="/plugin_assets/redmine_angular_ui/images/spinner.gif" ng-show="loading"/>',
+    link: function($scope, element, attrs) {
+      $scope.loading = true;
+      $scope.$watch("scopeVar", function() {
+        if ($scope.scopeVar !== undefined) {
+          $scope.loading = false;
+        } else {
+          $scope.loading = true;
+        }
+      });
+    }
+  };
+});
