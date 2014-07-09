c4e069f006b9b2dc4f2f05e69dc31e2d333a2205
Vincent Robert <vincent.robert@nanego.com>
2014-07-09 20:40:41 +0200

    Specify actions in push notifications

--- lib/redmine_angular_ui/issue_patch.rb
+++ lib/redmine_angular_ui/issue_patch.rb
@@ -1,8 +1,25 @@
 require_dependency 'issue'
 
 class Issue
-  after_commit do
-    json = {'text'=> self.id}.to_json
+
+  after_commit :notif_after_create, :on => :create
+  after_commit :notif_after_update, :on => :update
+  after_commit :notif_after_destroy, :on => :destroy
+
+  def notif_after_create
+    notif_after_commit('create')
+  end
+
+  def notif_after_update
+    notif_after_commit('update')
+  end
+
+  def notif_after_destroy
+    notif_after_commit('destroy')
+  end
+
+  def notif_after_commit(action)
+    json = {'issue_id'=> self.id, 'action'=>action}.to_json
     message = {:channel => '/issues', :data => json}
     uri = URI.parse("http://faye-redis.herokuapp.com/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
