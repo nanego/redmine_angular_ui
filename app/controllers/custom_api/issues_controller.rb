a4557a03b2bf90f7a9d4e7c74554975637548d47
Vincent Robert <vincent.robert@nanego.com>
2014-10-08 16:19:43 +0200

    Fix a bug when using class_name

--- app/controllers/custom_api/issues_controller.rb
+++ app/controllers/custom_api/issues_controller.rb
@@ -5,7 +5,7 @@ class CustomApi::IssuesController < ApplicationController
   def get_last_note
 
     # TODO Make it with only one request
-    sql = Journal.select('max(id) max_id, journalized_id, count(journalized_id), max(created_on) max_date').where("journalized_type = ? AND journalized_id IN (?) AND notes IS NOT NULL AND notes <> '' ", Issue.class_name, params['issue_ids']).group("journalized_id").to_sql
+    sql = Journal.select('max(id) max_id, journalized_id, count(journalized_id), max(created_on) max_date').where("journalized_type = ? AND journalized_id IN (?) AND notes IS NOT NULL AND notes <> '' ", Issue.to_s, params['issue_ids']).group("journalized_id").to_sql
     records = ActiveRecord::Base.connection.execute(sql)
 
     @issues_map = []
