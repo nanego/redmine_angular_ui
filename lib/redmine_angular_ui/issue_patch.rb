d6da28861c5430ec43f57ac240fd85cf9c5e6d0f
Vincent Robert <vincent.robert@nanego.com>
2014-10-09 16:11:36 +0200

    Temporarely disable production faye server

--- lib/redmine_angular_ui/issue_patch.rb
+++ lib/redmine_angular_ui/issue_patch.rb
@@ -22,7 +22,7 @@ class Issue
   def notif_after_commit(action)
     json = {'action'=>action, 'issue'=>{'id'=> self.id, 'priority' => {'id' => priority.id}, 'subject'=>subject ,'tracker'=>{'id'=>tracker.id, 'name'=>tracker.name}, 'project'=>{'id'=>project.id, 'name'=>project.name}, 'author'=>{'id'=>author.id, 'name'=>author.name}}}.to_json
     message = {:channel => '/issues', :data => json}
-    if Rails.env == 'development'
+    if 1==1 # Rails.env == 'development'
       faye_server_url = 'faye-redis.herokuapp.com/faye/faye'
     else
       faye_server_url = "#{Rails.application.routes.default_url_options[:host]}/faye/faye"
