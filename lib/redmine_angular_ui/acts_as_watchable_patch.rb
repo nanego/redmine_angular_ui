c54c0ba49680b6835d37caf216e1682d7e54693c
Vincent Robert <vincent.robert@nanego.com>
2014-10-30 14:49:42 +0100

    Patch method must return an Integer so redmine core tests can pass

--- lib/redmine_angular_ui/acts_as_watchable_patch.rb
+++ lib/redmine_angular_ui/acts_as_watchable_patch.rb
@@ -9,7 +9,7 @@ module Redmine
         # Removes user from the watchers list # Custom: use destroy_all instead of delete_all so we can use after_commit on a watcher object
         def remove_watcher(user)
           return nil unless user && user.is_a?(User)
-          watchers.where(:user_id => user.id).destroy_all
+          return watchers.where(:user_id => user.id).destroy_all.size
         end
 
       end
