dba4afc468384963ff4a02eefd0be3470f269458
Vincent Robert <vincent.robert@nanego.com>
2014-11-06 16:25:49 +0100

    Remove a misplaced hook. CSS files from this plugin must be included only to the new interface

--- lib/redmine_angular_ui/hooks.rb
+++
@@ -1,7 +0,0 @@
-module RedmineAngularUi
-  class Hooks < Redmine::Hook::ViewListener
-    def view_layouts_base_html_head(context)
-      stylesheet_link_tag("angular_ui", :plugin => "redmine_angular_ui")
-    end
-  end
-end
