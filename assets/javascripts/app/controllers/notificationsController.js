011a9fddb8e430f1bf140b65cf656c45e234d91c
Vincent Robert <vincent.robert@nanego.com>
2014-11-27 17:11:37 +0100

    Make the issue title bolder if the issue has been updated recently

--- assets/javascripts/app/controllers/notificationsController.js
+++ assets/javascripts/app/controllers/notificationsController.js
@@ -5,4 +5,5 @@ var app = angular.module('myApp.controllers');
 app.controller('NotificationsController', function($scope, NotificationService) {
   $scope.notifications = NotificationService.list;
   $scope.notified_lines = NotificationService.notified_lines;
+  $scope.recently_notified_lines = NotificationService.recently_notified_lines;
 });
--- assets/javascripts/app/services/notification.js
+++ assets/javascripts/app/services/notification.js
@@ -6,6 +6,7 @@ app.factory('NotificationService',function($timeout) {
   var service = {
     list: {},
     notified_lines: [],
+    recently_notified_lines: [],
     add: function (text, undo, delay, line) {
       var timestamp = (new Date()).getTime();
       service.list[timestamp] = {
@@ -21,10 +22,14 @@ app.factory('NotificationService',function($timeout) {
         }
       };
       service.notified_lines.push(line);
+      service.recently_notified_lines.push(line);
       $timeout(function () {
         delete service.list[timestamp];
         service.notified_lines.splice( service.notified_lines.indexOf(line), 1 );
-      }, (delay || 5) * 1000);
+      }, (delay || 5) * 1000); // custom delay or 5 seconds
+      $timeout(function () {
+        service.recently_notified_lines.splice( service.recently_notified_lines.indexOf(line), 1 );
+      }, 3600 * 1000); // 1 hour
     }
   };
   return service;
