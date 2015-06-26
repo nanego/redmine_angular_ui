219e4739d9925410ccc707add7d19ebd35ceba73
Vincent Robert <vincent.robert@nanego.com>
2015-06-26 08:26:02 +0200

    Add missing fixtures and update specs

--- spec/controllers/angular_controller_spec.rb
+++ spec/controllers/angular_controller_spec.rb
@@ -2,10 +2,12 @@ require "spec_helper"
 
 describe AngularController do
 
+  fixtures :users
+
   it "should should render the index view with the angular template" do
     @request.session[:user_id] = 1
     get :index
-    expect(response).to be_success
+    expect(response).to have_http_status(:ok) # should return a 200 OK status
     assert_template :index
     assert_template "layouts/angular"
   end
@@ -14,7 +16,7 @@ describe AngularController do
     # reset current user
     User.current = nil
     get :index
-    expect(response).to be_redirect
+    expect(response).to have_http_status(:redirect) # redirect (302)
   end
 
 end
