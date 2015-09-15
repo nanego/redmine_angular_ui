5d218f5c61ab5537ed1c30ba54dfc449b6248a58
Vincent Robert <vincent.robert@nanego.com>
2015-09-15 15:39:36 +0200

    Simplify html DOM

--- assets/javascripts/app/directives.js
+++ assets/javascripts/app/directives.js
@@ -209,7 +209,7 @@ app.directive('watchedToggle', function($timeout, $http) {
     restrict: 'E',
     replace: false,
     transclude: true,
-    template:	'<div class="fav-star" ng-click="toggle()"> <span class="fav icon"' +
+    template:	'<div ng-click="toggle()"> <span class="fav icon"' +
       'data-icon="{{ val == \'1\' ? \'S\' : \'s\' }}"> </span> </div>',
     link: function(scope, element, attrs) {
       if (!angular.isDefined(scope.ngWatched)) {
