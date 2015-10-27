1c527087013e0d77c6c9e557ad6988c468abbe02
Vincent Robert <vincent.robert@nanego.com>
2015-10-27 14:49:10 +0100

    Add a link on issues index pages

---
+++ app/overrides/issues/index.rb
@@ -0,0 +1,5 @@
+Deface::Override.new :virtual_path  => 'issues/index',
+                     :original      => '402b3096154c5706049c1de2f9fedeaa63f6b618',
+                     :name          => 'add-link-to-angular-client',
+                     :insert_before => 'div.contextual',
+                     :partial       => 'angular/link'
