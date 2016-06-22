3c8d7a93681f966d4edf5817d9d95b1273f4cd88
Vincent Robert <vincent.robert@nanego.com>
2016-06-22 14:45:54 +0200

    Add link to new interface on my/page

---
+++ app/overrides/my/page.rb
@@ -0,0 +1,4 @@
+Deface::Override.new :virtual_path  => 'my/page',
+                     :name          => 'add-link-to-angular-client',
+                     :insert_before => 'div.contextual',
+                     :partial       => 'angular/link'
