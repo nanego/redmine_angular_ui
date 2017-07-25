c772198554a1a7a4ab3e95dcb64e97eb2e4f3d2f
Vincent Robert <vincent.robert@nanego.com>
2017-07-25 14:54:09 +0200

    Temporary apply override to custom my-page

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -1,4 +1,4 @@
-Deface::Override.new :virtual_path  => 'my/page',
+Deface::Override.new :virtual_path  => 'my/custom_page',
                      :name          => 'add-link-to-angular-client',
                      :insert_before => 'div.contextual',
                      :partial       => 'angular/link'
